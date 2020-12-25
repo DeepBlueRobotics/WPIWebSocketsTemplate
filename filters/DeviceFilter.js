90ba3b5c76c17d38adf820a1865c3f7034ab1059
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2020-12-24 17:44:59 -0800

    javadocs

--- filters/DeviceFilter.js
+++ filters/DeviceFilter.js
@@ -14,4 +14,9 @@ function filterProps(props) {
     return props;
 }
 
-module.exports = { contains, filterProps }
+function aAn(str) {
+    //Credit: https://stackoverflow.com/questions/37896484/multiple-conditions-for-javascript-includes-method
+    return ['a', 'e', 'i', 'o', 'u'].some(el => str.includes(el));
+}
+
+module.exports = { contains, filterProps, aAn }
